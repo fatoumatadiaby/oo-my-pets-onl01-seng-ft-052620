a0e76b1b885cb1df6ba0b981781eebef76da67f6
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-16 03:38:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor :name 
+
+ 
+  def initialize(name)
+  @name = name
 end
+ 
+end
+
+
+# describe Owner do
+
+#   before do
+#     @owner = Owner.new("Victoria")
+#   end
+
+#   context 'Instance methods' do
+
+#     describe "#name" do
+#       it "can have a name" do
+#         expect(@owner.name).to eq("Victoria")
+#       end
+
+#       it "cannot change owner's name" do
+#         expect { @owner.name = "Beckham" }.to raise_error NoMethodError
+#       end
+#     end
