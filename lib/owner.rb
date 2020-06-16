19229d2690a514860a948cb127d8d1b05e048674
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-16 03:44:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,25 +4,8 @@ class Owner
   @name = name
  end
   def species
-    @species 
+    @species= (species)
   end
 end
 
 
-# describe Owner do
-
-#   before do
-#     @owner = Owner.new("Victoria")
-#   end
-
-#   context 'Instance methods' do
-
-#     describe "#name" do
-#       it "can have a name" do
-#         expect(@owner.name).to eq("Victoria")
-#       end
-
-#       it "cannot change owner's name" do
-#         expect { @owner.name = "Beckham" }.to raise_error NoMethodError
-#       end
-#     end
